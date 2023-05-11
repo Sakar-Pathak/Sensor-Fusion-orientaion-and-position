4ca5e0ae1e5fa5e5c7dc743834cc702d904e2ac7
Sakar Pathak <075bei030.sakar@pcampus.edu.np>
2023-05-11 22:49:04 +0545

    EKF source file in C++

--- Documents/Arduino/libraries/major_project/python_ahrs/config/config.prc
+++
@@ -1,5 +0,0 @@
-win-size 1000 750
-window-title Orientation Simulator
-sync-video true
-show-frame-rate-meter true
-task-timer-verbose #t
--- Documents/Arduino/libraries/major_project/python_ahrs/run_ekf_ahrs.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-::This is a simple batch file that runs the ekf_ahrs.py
-TITLE EKF AHRS
-ECHO Running ekf_ahrs.py...........
-call "C:\Users\Sakar\Documents\Arduino\libraries\major_project\python_ahrs\venv\Scripts\activate.bat"
-python "C:\Users\Sakar\Documents\Arduino\libraries\major_project\python_ahrs\ekf_ahrs.py"
-PAUSE
